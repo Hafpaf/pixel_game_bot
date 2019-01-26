6982cb59dd6c2609f12d66ef3665eb528643db9f
hafpaf <haf@hafnium.me>
2019-01-26 18:06:40 +0100

    change from directKeys to pyautogui

--- pixel.py
+++ pixel.py
@@ -2,7 +2,7 @@
 import numpy as np
 import pyscreenshot as ImageGrab
 import cv2
-from directKeys import click, queryMousePosition
+from pyautogui import click, position
 import time
 
 gameCoords = [corner1, corner2, corner3, corner4] #coordinats to play within
@@ -11,7 +11,7 @@ screen = np.array(ImageGrab.grab(bbox=gameCoords))
 screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
 while True:
-    mousePos = queryMousePosition()
+    mousePos = position()
     if gameCoords[2] > mousePos.x > gameCoords[0]:
         startTime = time.time()
         for y in range(len(screen)):
