da98d3517f487c63015a00cd3afd13e0f9734d90
hafpaf <haf@hafnium.me>
2019-01-27 11:48:41 +0100

    moved screen variable into while loop

--- pixel.py
+++ pixel.py
@@ -4,13 +4,11 @@ import sys
 import numpy as np
 import pyscreenshot as ImageGrab
 import cv2
-from pyautogui import click, position
+from pyautogui import click, position, onScreen, FAILSAFE, hotkey
 import time
 
 FAILSAFE() = True
 
-screen = np.array(ImageGrab.grab(bbox=gameCoords))
-screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
 gameCoords = [463, 107, 890, 535] #x and y coordinats to play within
 
@@ -19,11 +17,13 @@ while True:
     mousePos = position()
     if gameCoords[2] > mousePos.x > gameCoords[0]:
         startTime = time.time()
+        screen = np.array(ImageGrab.grab(bbox=gameCoords))
+        screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
         for y in range(len(screen)):
             for x in range(len(screen[y])):
                 if screen[y][x] < 10:
-                    #click(x, y)
+                    click(x, y)
                     pass
-    print("Frame tooke {} seconds".format((time.time() - startTime, i)))
+        print("Frame took {} seconds, hey ther {}".format((time.time() - startTime), "user"))
 
 exit(esc) #Escape program
