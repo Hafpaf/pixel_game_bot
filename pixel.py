2d24a4a409b9f1fd8908ef255e1f7d8fc13c4673
hafpaf <haf@hafnium.me>
2019-01-27 11:46:42 +0100

    import more sys libary + comments

--- pixel.py
+++ pixel.py
@@ -1,4 +1,6 @@
 #Part of code by CodeBullet: https://youtu.be/wHRubMACen0
+#game: http://www.donttap.com/
+import sys
 import numpy as np
 import pyscreenshot as ImageGrab
 import cv2
@@ -11,6 +13,7 @@ screen = np.array(ImageGrab.grab(bbox=gameCoords))
 screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
 
 while True:
+#    onScreen(x, y)
     mousePos = position()
     if gameCoords[2] > mousePos.x > gameCoords[0]:
         startTime = time.time()
